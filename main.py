b50abe31c892c732462bf77d8c278bf24c6b431c
Yusuf <yusuf.a@demandnxt.com>
2023-06-12 09:38:47 +0530

    few changes

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ if WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it(
     # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
     #     (By.CSS_SELECTOR, "div.recaptcha-checkbox-border"))).click()
     captcharesolver()
+    
 else:
     os.exit()
     # myButton = driver.find_element(By.CSS_SELECTOR, ".btn-primary" )
