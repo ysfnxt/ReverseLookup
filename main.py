7af38e22b7775017dc0df9c529694d655e41f252
Yusuf <yusuf.a@demandnxt.com>
2023-06-15 16:42:40 +0530

    printing captcha

--- main.py
+++ main.py
@@ -43,8 +43,6 @@ if WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it(
     #     (By.CSS_SELECTOR, "div.recaptcha-checkbox-border"))).click()
     captcha_resolver(driver)
     
-    # driver.execute_script("""document.querySelector('[name="g-recaptcha-response"]').innerText='{}'""".format(final_req))
-    
 else:
     os.exit()
     # myButton = driver.find_element(By.CSS_SELECTOR, ".btn-primary" )
